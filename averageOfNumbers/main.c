7e3421997f478da5b1d5e41b55fbd01fb6608a72
Afton Lawver <lawverap25@gmail.com>
2022-09-28 14:42:25 -0500

    Finished function for CS 335 (Operating Systems). Finds the average of numbers input by user.

---
+++ averageOfNumbers/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+    //Initialize variables
+    int input;
+    int count = 0;
+    int sum = 0;
+
+    printf("Enter a positive integer number or enter -1 to quit and calculate average: ");
+    scanf("%d", &input);
+    while(input != -1) {
+        count +=1;
+        sum += input;
+        printf("Enter another positive integer or enter -1 to quit and calculate average: ");
+        scanf("%d", &input);
+        continue;
+    }
+    if (count == 0) {
+        printf("No numbers to calculate average with.");
+        return 1;
+    }
+
+    double average = (double)sum / (double)count;
+
+    printf("The average of these numbers is: %f\n", average);
+    printf("Press ENTER key to Continue\n");
+    char button;
+    fflush(stdin);
+    scanf("%c", &button);
+    if (button == 10) {
+        return 0;
+    }
+
+
+}
+
