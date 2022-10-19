21955f7c13f1677a972489c6d24c2d28ebee4bf7
Afton Lawver <lawverap25@gmail.com>
2022-10-19 14:26:52 -0500

    Almost done with Task 3 (using lseek system call).

--- CS335_Task3_lseek/main.c
+++ CS335_Task3_lseek/main.c
@@ -6,7 +6,6 @@
 
 
 int main(int argc, char *argv[]) {
-    int f;
     if (argc > 2) {
         printf("Too many arguments supplied. \n");
         exit(0);
@@ -16,25 +15,14 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
     else {
-        pid_t  pid;
-        /* fork another process */
-        pid = fork();
-
-        if (pid < 0) { /* error occurred */
-            fprintf(stderr, "Fork Failed");
-            exit(-1);
+        int f;
+        if( (f = open(argv[1], O_RDONLY) ) < 0 )
+        {
+            perror( "open failed" );
+            exit( 1 );
         }
-        else if (pid == 0) { /* child process */
-            f = open("seek", O_RDWR);
-            lseek(f, 5, SEEK_CUR);
-        }
-        else { /* parent process */
-            /* parent will wait for the child to complete */
-            wait (NULL);
-            printf ("Child Complete\n");
-            return 0;
-        }
-
+        int size = lseek(f, 0, SEEK_END);
+        printf("The size of the file is: %d\n", size);
+        close(f);
     }
-
 }
