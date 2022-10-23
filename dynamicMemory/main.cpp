a5de35f43a64fcb30c9af3d36b10dc9dfa5ab19c
Afton Lawver <lawverap25@gmail.com>
2022-10-23 17:05:21 -0500

    Finished exercise #1 for CS 304 (dynamic memory allocation).

--- dynamicMemory/main.cpp
+++ dynamicMemory/main.cpp
@@ -5,26 +5,47 @@ bool isEven(int number);
 
 int main() {
     int input;
-    int *dynamicIntArray = (int*) malloc(10 * sizeof(int));
-    int *size;
-    int s = 10;
-    size = &s;
-    int iteration = 1;
+    int size = 10;
+    int *dynamicIntArray = (int*) realloc(NULL, size * sizeof(int));
+    int index = 0;
 
     cout << "Input a valid positive integer, or 0 to quit: " << endl;
+    while(cin >> input) {
+        if (input >= 1) {
+            dynamicIntArray[index] = input;
 
-    while(cin >> input && input >= 1) {
 
-        dynamicIntArray[iteration] = input;
-        cout << "Input another valid positive integer, or 0 to quit: " << endl;
-
+            if (index == size) {
+                size = size * 2;
+                int *dynamicIntArray = (int*) realloc(dynamicIntArray,size * sizeof(int));
+                cout << "Array doubled in size." << endl;
+                continue;
+            }
+            index ++;
+            cout << "Input another valid positive integer, or 0 to quit: " << endl;
+        }
+        else {
+            int count = 0;
+            for (int i = 0; i<index; i++) {
+                if (dynamicIntArray[i] == 0) {
+                    break;
+                }
+                else if (isEven(dynamicIntArray[i])) {
+                    count ++;
+                }
+            }
+            cout << "Number of numbers that are even: " << count << endl;
+        }
     }
-
     return 0;
 }
 
 bool isEven(int number) {
-    if(number % 2 ==0) {
+    if (number == 0) {
+        return false;
+    }
+    else if (number % 2 == 0) {
         return true;
     }
+    return false;
 }
