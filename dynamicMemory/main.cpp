2ae1752233a2a415ed35eaec895f48e70c56d7d3
Afton Lawver <lawverap25@gmail.com>
2022-10-28 14:45:26 -0500

    Modified dynamic memory Exercise #1 for CS 304. (Added some exception handling in case malloc or realloc is unsuccessful)

--- dynamicMemory/main.cpp
+++ dynamicMemory/main.cpp
@@ -9,20 +9,30 @@ int main() {
     int *dynamicIntArray = (int*) realloc(NULL, size * sizeof(int));
     int index = 0;
 
+    if(dynamicIntArray == NULL) {
+        cout << "malloc() failed!" << endl;
+        return 1;
+    }
+
     cout << "Input a valid positive integer, or 0 to quit: " << endl;
     while(cin >> input) {
         if (input >= 1) {
             dynamicIntArray[index] = input;
 
 
-            if (index == size) {
+            if (index+1 == size) {
+                cout << "Reallocating the buffer from " << size << " to " << size*2 << endl;
                 size = size * 2;
-                int *dynamicIntArray = (int*) realloc(dynamicIntArray,size * sizeof(int));
-                cout << "Array doubled in size." << endl;
-                continue;
+                dynamicIntArray = (int*) realloc(dynamicIntArray,size * sizeof(int));
+                if(dynamicIntArray == NULL) {
+                    cout << "realloc() failed" << endl;
+                    return 1;
+                }
+                cout << "Array has doubled in size." << endl;
             }
             index ++;
             cout << "Input another valid positive integer, or 0 to quit: " << endl;
+            continue;
         }
         else {
             int count = 0;
@@ -35,6 +45,7 @@ int main() {
                 }
             }
             cout << "Number of numbers that are even: " << count << endl;
+            break;
         }
     }
     return 0;
