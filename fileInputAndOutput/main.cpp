7e8d986c8941b7eca21a6a4c590456915ccdac95
Afton Lawver <lawverap25@gmail.com>
2022-11-02 17:13:12 -0500

    Played with file input and output streams using fstream class.

---
+++ fileInputAndOutput/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    fstream fileStream;
+    fileStream.open("C:\\CProgramming\\fileInputAndOutput\\test.txt");
+
+    for(int i = 0; i < 100; i++) {
+        int temp;
+        temp = fileStream.get();
+        if(temp == -1){
+            break;
+        }
+        cout << (char) temp << endl;
+    }
+
+    return 0;
+}
