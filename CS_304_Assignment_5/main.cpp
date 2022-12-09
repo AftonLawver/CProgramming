3bbec3c5748b7207623ad5d4eab638e139e7e2a2
Afton Lawver <lawverap25@gmail.com>
2022-12-09 14:46:53 -0600

    Modified code to run on linux command line. Need to test/debug for memory allocation.

--- CS_304_Assignment_5/main.cpp
+++ CS_304_Assignment_5/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <unistd.h>
 #include <map>
+#include <string.h>
 
 using namespace std;
 
@@ -80,66 +81,57 @@ public:
 };
 
 //int argc, char *argv[]
-int main() {
+int main(int argc, char *argv[]) {
     unsigned int number_of_characters = 0;
     unsigned int number_of_lines = 1;
     string file_name;
     // have a variable called number of files that grabs argc-1 and sets buffer size = that
-    int buffer_size = 1;
     char **buffer;
-    buffer = (char **) malloc(buffer_size * sizeof(char*));
+    int number_of_files = argc - 1;
+    int buffer_size = number_of_files;
+    buffer = (char **)malloc(buffer_size * sizeof(char *));
     if (buffer == NULL) {
         cout << "Error in allocating memory!!" << endl;
         return -1;
     }
-    int buffer_index = 0;
 
-//    if(argc == 1) {
-//        cout << "Enter at least one file as the command line argument. Please try the program again." << endl;
-//        sleep(2);
-//        exit(0);
-//    }
-//    cout << "Number of args: " << argc << endl;
-    cout << "Enter filename:" << endl;
-//    for (int i=1; i<argc; i++) {
-//        string file_name = argv[i];
-// cout << "File path: " << file_name << endl;
-// cout << "-------------------------------" << endl
-    cin >> file_name;
-    word_list my_word_list;
-    fstream file_stream;
-    file_stream.open(file_name, ios::in);
-//    if(file_stream.fail()){
-//        cout << "File does not exist. Please try again." << endl;
-//        sleep(2);
-//        exit(0);
-//    }
-    string current_word;
-
-//C:\Users\lawve\Desktop\test.txt
-    while (true) {
+    if (argc == 1) {
+        cout << "Enter at least one file as the command line argument. Please try the program again." << endl;
+        sleep(2);
+        exit(0);
+    }
+//    cout << "Enter filename:" << endl;
+    for (int i = 1; i < argc; i++) {
+        string file_name = argv[i];
+//    cin >> file_name;
+        word_list my_word_list;
+        fstream file_stream;
+        file_stream.open(file_name, ios::in);
+    if(file_stream.fail()){
+        cout << "File does not exist. Please try again." << endl;
+        sleep(2);
+        exit(0);
+    }
+        string current_word;
+        while (true) {
             if (file_stream.peek() == 32 || file_stream.peek() == 10 || file_stream.peek() == -1) {
-                if(file_stream.peek() == -1) {
+                if (file_stream.peek() == -1) {
                     if (current_word == "") {
                         break;
-                    }
-                    else{
+                    } else {
                         word new_word = word(current_word);
                         my_word_list.add_word(new_word);
                         break;
                     }
-
                 }
-
-                // newline
+                    // newline
                 else if (file_stream.peek() == 10) {
                     if (current_word == "") {
                         int x = file_stream.get(); // consumes the newline character
                         number_of_lines++;
                         number_of_characters++;
                         continue;
-                    }
-                    else {
+                    } else {
                         word new_word = word(current_word);
                         my_word_list.add_word(new_word);
                         int x = file_stream.get(); // consumes the newline
@@ -148,15 +140,12 @@ int main() {
                         number_of_characters++;
                         continue;
                     }
-                }
-
-                else if (file_stream.peek() == 32) {
+                } else if (file_stream.peek() == 32) {
                     if (current_word == "") {
                         int x = file_stream.get(); // consumes the whitespace
                         number_of_characters++;
                         continue;
-                    }
-                    else {
+                    } else {
                         word new_word = word(current_word);
                         my_word_list.add_word(new_word);
                         int x = file_stream.get(); // consumes the whitespace
@@ -173,39 +162,6 @@ int main() {
         if (number_of_characters == 0) {
             number_of_lines = 0;
         }
-        // Need to add all of this information into dynamic memory to be printed out at the end.
-
-    string long_string = "Name of file: " + file_name + "\n"
-                         + "Number of words: " + to_string(my_word_list.get_number_of_words()) + "\n"
-                         + "Number of characters: " + to_string(number_of_characters) + "\n"
-                         + "Number of lines: " + to_string(number_of_lines) + "\n"
-                         + "Number of words beginning with each letter: \n"
-                         + "------------------------------------------\n"
-                         + my_word_list.get_number_of_words_for_each_letter() + "\n";
-
-
-    char *c_string = (char*)(long_string.c_str());
-
-    // allocate space within buffer
-    for (int i = 0; i < buffer_size; i++) {
-            buffer[i] = (char*)malloc(long_string.length() * sizeof(char));
-            buffer[i] = c_string;
-    }
-
-    // print all file information from buffer
-    for (int j = 0; j < buffer_size; j++) {
-        cout<< buffer[j];
-        cout << endl;
-    }
-
-    // free dynamically allocated memory
-    for (int j = 0; j < buffer_size; j++) {
-        free(buffer[j]);
-    }
-    free(buffer);
-    cout << "All dynamic memory has been freed." << endl;
-
-//C:\CProgramming\test.txt
 //        cout << "Name of file: " << file_name << endl;
 //        cout << "Number of words: " << my_word_list.get_number_of_words() << endl;
 //        cout << "Number of characters: " << number_of_characters << endl;
@@ -215,54 +171,34 @@ int main() {
 //        cout << "------------------------------------------" << endl;
 //        string result = my_word_list.get_number_of_words_for_each_letter();
 //        cout << result << endl;
+        // Need to add all of this information into dynamic memory to be printed out at the end.
 
-        return 0;
+        string long_string = "Name of file: " + file_name + "\n"
+                             + "Number of words: " + to_string(my_word_list.get_number_of_words()) + "\n"
+                             + "Number of characters: " + to_string(number_of_characters) + "\n"
+                             + "Number of lines: " + to_string(number_of_lines) + "\n"
+                             + "Number of words beginning with each letter: \n"
+                             + "------------------------------------------\n"
+                             + my_word_list.get_number_of_words_for_each_letter() + "\n";
+
+        char *c_string = (char *) (long_string.c_str());
+
+        // allocate space within buffer and add data to the buffer
+        buffer[i - 1] = (char *) malloc(long_string.length() * sizeof(char));
+        buffer[i - 1] = c_string;
     }
 
-//        int peek_value = file_stream.peek();
-//        switch (peek_value) {
-//            case 32: {
-//                if (current_word == "") {
-//                    int x = file_stream.get(); // consumes the whitespace
-//                    number_of_characters++;
-//                    continue;
-//                }
-//                word new_word = word(current_word);
-//                my_word_list.add_word(new_word);
-//                int x = file_stream.get(); // consumes the whitespace
-//                current_word = ""; // sets the current word back to empty string
-//                number_of_characters++;
-//                continue;
-//            }
-//            case 10: {
-//                if (current_word == "") {
-//                    int x = file_stream.get(); // consumes the newline character
-//                    number_of_lines++;
-//                    number_of_characters++;
-//                    continue;
-//                }
-//                word new_word = word(current_word);
-//                my_word_list.add_word(new_word);
-//                int x = file_stream.get(); // consumes the newline
-//                current_word = ""; // sets the current word back to empty string
-//                number_of_lines++;
-//                number_of_characters++;
-//                continue;
-//            }
-//            case -1: {
-//                if (current_word == "") {
-//                    processing_done = true;
-//                    break;
-//                }
-//                word new_word = word(current_word);
-//                my_word_list.add_word(new_word);
-//                processing_done = true;
-//                break;
-//            }
-//            default:
-//                char current_char = file_stream.get();
-//                number_of_characters++;
-//                current_word += current_char;
+    // print all file information from buffer
+    for (int j = 0; j < buffer_size; j++) {
+        cout << buffer[j];
+        cout << endl;
+    }
 
-//        }
+    // free dynamically allocated memory
+    free(buffer);
+    cout << "All dynamic memory has been freed." << endl;
 
+//C:\CProgramming\test.txt
+    return 0;
+
+}
