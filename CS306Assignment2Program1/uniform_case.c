1962daa4bebbf183a4ca3e7d3b1384700b6f9f3a
Afton Lawver <lawverap25@gmail.com>
2023-03-09 14:22:57 -0600

    Finished Program #1 on Assignment #2 for CS306. Need to test on terminal.

--- CS306Assignment2Program1/uniform_case.c
+++ CS306Assignment2Program1/uniform_case.c
@@ -7,10 +7,11 @@
 #include <stdlib.h>
 
 int isUpper(int inputCharacter);
-
+char *convertToUpper(char *inputString, char *outputString);
+char *convertToLower(char *inputString, char *outputString);
 
 int main(int argc, char *argv[]) {
-    int x = argv[1][0];
+    int firstCharacter = argv[1][0];
 
     // allocate new array for new string in memory
     char *ptr = malloc((strlen(argv[1])) * sizeof(char));
@@ -18,22 +19,13 @@ int main(int argc, char *argv[]) {
         perror("malloc() failed");
         exit(EXIT_FAILURE);
     }
-    if (isUpper(x) == 1) {
+    if (isUpper(firstCharacter) == 1) {
         printf("First letter is uppercase.\n");
-        for (int i = 0; argv[1][i] != '\0'; i++) {
-            ptr[i] = *argv[i] + 32;
-        }
-        int stringLength = strlen(ptr);
-        ptr[stringLength] = '\0';
-
+        ptr = convertToUpper(argv[1], ptr);
     }
     else {
         printf("First letter is lowercase.\n");
-        for (int i = 0; argv[1][i] != '\0'; i++) {
-            ptr[i] = *argv[i] - 32;
-        }
-        int stringLength = strlen(ptr);
-        ptr[stringLength] = '\0';
+        ptr = convertToLower(argv[1], ptr);
     }
     printf("New String: %s", ptr);
     return 0;
@@ -48,6 +40,24 @@ int isUpper(int inputCharacter) {
     }
 }
 
-int convertToUpper(char inputCharacter) {
-    int upperCaseCharacter = inputCharacter + 32;
+char *convertToUpper(char *inputString, char *outputString) {
+    outputString[0] = inputString[0];
+    int i = 1;
+    while (inputString[i] != '\0') {
+        outputString[i] = inputString[i] - 32;
+        i++;
+    }
+    outputString[strlen(outputString)-1] = '\0';
+    return outputString;
+}
+
+char *convertToLower(char *inputString, char *outputString) {
+    outputString[0] = inputString[0];
+    int i = 1;
+    while (inputString[i] != '\0') {
+        outputString[i] = inputString[i] + 32;
+        i++;
+    }
+    outputString[strlen(outputString)-1] = '\0';
+    return outputString;
 }
