936b037f08e5d470c2be9f81986e548fc9684353
Afton Lawver <lawverap25@gmail.com>
2023-03-08 21:50:05 -0600

    Initial implementation of Program #1 in CS306 Assignment #2. (working with Cstrings and dynamic memory).

--- CS306Assignment2Program1/uniform_case.c
+++ CS306Assignment2Program1/uniform_case.c
@@ -2,8 +2,52 @@
 // SIU851463587
 
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int isUpper(int inputCharacter);
+
 
 int main(int argc, char *argv[]) {
-    printf("The argument supplied is");
+    int x = argv[1][0];
+
+    // allocate new array for new string in memory
+    char *ptr = malloc((strlen(argv[1])) * sizeof(char));
+    if (ptr == NULL) {
+        perror("malloc() failed");
+        exit(EXIT_FAILURE);
+    }
+    if (isUpper(x) == 1) {
+        printf("First letter is uppercase.\n");
+        for (int i = 0; argv[1][i] != '\0'; i++) {
+            ptr[i] = *argv[i] + 32;
+        }
+        int stringLength = strlen(ptr);
+        ptr[stringLength] = '\0';
+
+    }
+    else {
+        printf("First letter is lowercase.\n");
+        for (int i = 0; argv[1][i] != '\0'; i++) {
+            ptr[i] = *argv[i] - 32;
+        }
+        int stringLength = strlen(ptr);
+        ptr[stringLength] = '\0';
+    }
+    printf("New String: %s", ptr);
     return 0;
+}
+
+int isUpper(int inputCharacter) {
+    if (inputCharacter >= 65 && inputCharacter <= 91) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int convertToUpper(char inputCharacter) {
+    int upperCaseCharacter = inputCharacter + 32;
 }
