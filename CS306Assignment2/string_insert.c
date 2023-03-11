dc61a27c79e02c1e32cb20c89838f23c6b468db0
Afton Lawver <lawverap25@gmail.com>
2023-03-10 22:27:24 -0600

    Finished CS306 Assignment #2, Program #3. (working with and manipulating strings, char pointers).

--- CS306Assignment2/string_insert.c
+++ CS306Assignment2/string_insert.c
@@ -6,11 +6,10 @@
 #include <string.h>
 
 int string_length(char *stringInput);
-char* string_n_copy(char* destination, const char* source);
 
 int main(int argc, char *argv[]) {
     if (argc != 4) {
-        printf("Wrong number of arguments\n");
+        printf("Wrong number of arguments. Please enter 3 arguments.\n");
         return 1;
     }
     else {
@@ -21,14 +20,24 @@ int main(int argc, char *argv[]) {
         char* charPointer;
         if (integerArgument < lengthOfString1) {
             charPointer = (char *) malloc(sizeof(char) * (lengthOfString1 + lengthOfString2 + 1));
+            int currentPositionFirstString = 0;
+            int currentPositionSecondString = 0;
             for (int i = 0; i < totalLength; i++) {
-                if (i == integerArgument) {
-                    charPointer = strcpy(charPointer, argv[3]);
-                    i+=lengthOfString2 + 1;
-                    printf("")
+                if (integerArgument > i) {
+                    charPointer[i] = argv[1][currentPositionFirstString];
+                    currentPositionFirstString++;
+                }
+                else if (i == integerArgument) {
+                    while (argv[3][currentPositionSecondString] != '\0') {
+                        charPointer[i] = argv[3][currentPositionSecondString];
+                        currentPositionSecondString++;
+                        i++;
+                    }
+                    i--;
                 }
                 else {
-                    printf("integer does not equal 0.\n");
+                    charPointer[i] = argv[1][currentPositionFirstString];
+                    currentPositionFirstString++;
                 }
             }
         }
@@ -36,6 +45,7 @@ int main(int argc, char *argv[]) {
             printf("Argument 2 is greater than the length of the first string\n");
             exit(EXIT_FAILURE);
         }
+        charPointer[totalLength] = '\0';
         printf("New string: %s\n", charPointer);
         return 0;
     }
@@ -47,26 +57,5 @@ int string_length(char *stringInput) {
         i++;
     }
     return i;
-}
-
-char* string_n_copy(char* destination, const char* source)
-{
-    if (destination == NULL) {
-        return NULL;
-    }
-
-    // take a pointer pointing to the beginning of the destination string
-    char *ptr = destination;
-
-    // copy the C-string pointed by source into the array
-    // pointed by destination
-    while (*source != '\0')
-    {
-        *destination = *source;
-        destination++;
-        source++;
-    }
-    *destination = '\0';
-    return ptr;
 }
 
