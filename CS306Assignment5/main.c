55a5ee6495b331c9be949e629e4ac67f8c1ca855
Afton Lawver <lawverap25@gmail.com>
2023-04-28 13:15:45 -0500

    Figured out how to count the number of bytes in each file.

--- CS306Assignment5/main.c
+++ CS306Assignment5/main.c
@@ -12,57 +12,60 @@ struct args {
 
 // where the thread will read in some part of the file
 void *threadhandler(void *input) {
-//    int fd, n;
-//    char buf[1024];
-//
-//    fd = open(arg, O_RDONLY);
-//    if (fd < 0) {
-//        printf("Error opening the file.");
-//        return (void *) 1;
-//    }
-//
-//    n = read(fd, buf, sizeof(buf));
-//    close(fd);
-    printf("filename: %s\n", ((struct args*)input)->filename);
-    printf("index of file in array: %d\n", ((struct args*)input)->index);
+    int n;
+    char c;
+    const char *filename = (((struct args *) input)->filename);
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        perror("File does not exist\n");
+        return (void *) 1;
+    } else {
+        printf("File was opened successfully\n");
+        unsigned long count = 0;
 
-//    pthread_mutex_lock(&mutex);
-//
-//    pthread_mutex_unlock(&mutex);
-    return NULL;
-}
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        printf("Invalid number of arguments. Use at least two args.\n");
-        return 1;
-    }
-    else {
-        // array of character pointers to hold filenames
-        const char *filenames[argc-1];
-        for (int i=0; i<argc-1; i++) {
-            filenames[i] = argv[i+1];
+        while ((n = read(fd, &c, 1)) > 0) {
+            if (c != EOF) {
+                count += n;
+            }
         }
+        printf("filename: %s\n", ((struct args *) input)->filename);
+        printf("index of file in array: %d\n", ((struct args *) input)->index);
 
-        // dynamically allocated array of unsigned longs to hold bytes of each file
-        unsigned long *numberOfBytes = (unsigned long*) malloc((argc-1)*sizeof(unsigned long));
+        return NULL;
+    }
+}
+    int main(int argc, char *argv[]) {
+        if (argc < 3) {
+            printf("Invalid number of arguments. Use at least two args.\n");
+            return 1;
+        }
+        else {
+            // array of character pointers to hold filenames
+            const char *filenames[argc - 1];
+            for (int i = 0; i < argc - 1; i++) {
+                filenames[i] = argv[i + 1];
+            }
 
-        // create a thread for each file
-        for (int j=1; j<argc; j++) {
-            struct args *file = (struct args *)malloc(sizeof(struct args));
+            // dynamically allocated array of unsigned longs to hold bytes of each file
+            unsigned long *numberOfBytes = (unsigned long *) malloc((argc - 1) * sizeof(unsigned long));
+
+            // create a thread for each file
+            for (int j = 1; j < argc; j++) {
+                struct args *file = (struct args *) malloc(sizeof(struct args));
 //            char allen[] = "Allen";
-            file->filename = argv[j];
-            file->index = j-1;
-            pthread_t tid;
-            pthread_create(&tid,NULL,threadhandler,(void *)file);
-            pthread_join(tid, NULL);
-        }
+                file->filename = argv[j];
+                file->index = j - 1;
+                pthread_t tid;
+                pthread_create(&tid, NULL, threadhandler, (void *) file);
+                pthread_join(tid, NULL);
+            }
 
-        exit(EXIT_SUCCESS);
+            exit(EXIT_SUCCESS);
 
-        // print the number of bytes for each file
-        for (int i=0; i<argc-1; i++) {
-            printf("%s\n", filenames[i]);
+            // print the number of bytes for each file
+            for (int i = 0; i < argc - 1; i++) {
+                printf("%s\n", filenames[i]);
+            }
         }
+        return 0;
     }
-    return 0;
-}
